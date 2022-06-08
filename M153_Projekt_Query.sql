3b5967e24da4312e9d5619a5760a340adecfd439
Benjamin Schleiss <benjamin@kauz.ch>
2022-06-08 21:28:51 +0200

    Created a Select and Update querry

---
+++ M153_Projekt_Query.sql
@@ -0,0 +1,14 @@
+use Game_Datenbank
+go
+
+--Durchschnitts Spielzeit pro Genre
+Select isNull(Avg( dauer),0 )as 'Durchschnitts Spielzeit', Genre.bezeichnung as Genre
+from Game
+    join GameGenre on fk_gameID=Game.id
+    RIGHT join Genre on fk_genreID=Genre.id
+GROUP by Genre.id, Genre.bezeichnung
+
+--Alle spiele über 100 stunden Empfehlen
+UPDATE Game 
+set empfohlen =1 
+WHERE dauer >100
